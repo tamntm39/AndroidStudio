9d5a8600db0e6af2128e980d17f6d64dba046611
Duck Hải <letranduchai2004@gmail.com>
2024-01-23 21:46:50 +0700

    done layout home

---
+++ app/src/main/java/com/example/tabselector/HomeActivity.java
@@ -0,0 +1,35 @@
+package com.example.tabselector;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class HomeActivity extends AppCompatActivity {
+
+   Button btnDangKy, btnLoginApple, btnLoginSdt, btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //anh xa
+        setContentView(R.layout.activity_home);
+        btnDangKy = findViewById(R.id.btnDangKy);
+        btnLoginApple = findViewById(R.id.btnLoginApple);
+        btnLoginSdt = findViewById(R.id.btnLoginSdt);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
